ffda0e5f816d0d75834378371f1c2a4bd3c1c809
Supakit00 <93958629+Supakit00@users.noreply.github.com>
2021-12-14 10:40:06 +0700

    5555

--- array.php
+++ array.php
@@ -15,12 +15,12 @@
     echo "Joe is" .  $cars['Joe'] . "years old.";
 
    //Multidimensional Arrays
-    $cars1 = array {
+    $cars1 = array (
         array ("Volvo" , 22 ,18),
         array ("BMW" ,15 ,13),
         array ("Saab" , 5 ,2),
         array ("Land Rrover" , 17 ,15)
-    };
+    );
     echo $cars1[0] [0] . ": In stock: ".  $cars[0] [1].", sold; ".  $cars[0] [2].".<br>";
     echo $cars1[1] [0] . ": In stock: ".  $cars[1] [1].", sold; ".  $cars[1] [2].".<br>";
     echo $cars1[2] [0] . ": In stock: ".  $cars[2] [1].", sold; ".  $cars[2] [2].".<br>";
---
+++ ืnumber.php
@@ -0,0 +1,35 @@
+<?php
+$number1 = $_POST["nember1"];
+$number2 = $_POST["nember2"];
+$number3 = $_POST["nember3"];
+$number4 = $_POST["nember4"];
+$number5 = $_POST["nember5"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form>action="number.php" method="POST">
+        number 1 : <input type ="number" name="number1"><br>
+        number 2 : <input type ="number" name="number2"><br>
+        number 3 : <input type ="number" name="number3"><br>
+        number 4 : <input type ="number" name="number4"><br>
+        number 5 : <input type ="number" name="number5"><br>
+        <input type="submit" value="send">
+        <input type="rest" value="reset">
+    </form>
+</body>
+</html>
+<?php
+        if(isset($number1)) {
+            $sum = $number1+$number1+$number2+$number3+$number4+$number5;
+            echo $sum;
+        }else{
+            echo "ไม่มีข้อมูล";
+        }
+?>
